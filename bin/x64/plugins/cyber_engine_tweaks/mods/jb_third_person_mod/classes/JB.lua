313ce736351ce4e6b1267c4845d87f8b8d8bec76
striderxfossility <jellebakker15@live.nl>
2021-02-08 10:49:08 +0100

    Created JB class

---
+++ bin/x64/plugins/cyber_engine_tweaks/mods/jb_third_person_mod/classes/JB.lua
@@ -0,0 +1,15 @@
+local JB = {}
+JB.__index = JB
+
+function JB:new()
+    local class = {}
+
+    ----------VARIABLES-------------
+
+    ----------VARIABLES-------------
+
+    setmetatable( class, JB )
+    return class
+end
+
+return JB:new()
