e8ebbb2f23c9389e0c87b083ba40641bb2043196
Jelle Bakker <jellebakker15@live.nl>
2021-11-21 15:30:45 +0100

    donno
    Signed-off-by: Jelle Bakker <jellebakker15@live.nl>

--- bin/x64/plugins/cyber_engine_tweaks/mods/jb_third_person_mod/init.lua
+++ bin/x64/plugins/cyber_engine_tweaks/mods/jb_third_person_mod/init.lua
@@ -169,24 +169,25 @@ registerForEvent("onUpdate", function(deltaTime)
             end
         end
 
-        if JB.isTppEnabled and not JB.inCar then
-            local PlayerSystem = Game.GetPlayerSystem()
-            local PlayerPuppet = PlayerSystem:GetLocalPlayerMainGameObject()
-            local ts = Game.GetTransactionSystem()
+	-- NOT WORKING ANYMORE OF 1.3
+        --if JB.isTppEnabled and not JB.inCar then
+            --local PlayerSystem = Game.GetPlayerSystem()
+            --local PlayerPuppet = PlayerSystem:GetLocalPlayerMainGameObject()
+            --local ts = Game.GetTransactionSystem()
         
-            local slotID = TweakDBID.new('AttachmentSlots.TppHead')
-            local item = ts:GetItemInSlot(PlayerPuppet, slotID)
+            --local slotID = TweakDBID.new('AttachmentSlots.TppHead')
+            --local item = ts:GetItemInSlot(PlayerPuppet, slotID)
         
-            if Gender:IsFemale() then
-                seamfix = PlayerPuppet:FindComponentByName(CName.new("t0_000_pwa_base__full_seamfix"))
-            else
-                seamfix = PlayerPuppet:FindComponentByName(CName.new("t0_000_pma_base__full_seamfix"))
-            end
+            --if Gender:IsFemale() then
+            --    seamfix = PlayerPuppet:FindComponentByName(CName.new("t0_000_pwa_base__full_seamfix"))
+            --else
+            --    seamfix = PlayerPuppet:FindComponentByName(CName.new("t0_000_pma_base__full_seamfix"))
+            --end
 
-            if not JB.ModelMod then
-                seamfix:Toggle(false)
-            end
-        end
+            --if not JB.ModelMod then
+            --    seamfix:Toggle(false)
+            --end
+        --end
     end
 end)
 
